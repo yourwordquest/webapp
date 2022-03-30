1bbe36ac30a39a2b8d3fe12fa54df08d8a807fde
Billcountry <me@billcountry.tech>
2022-03-30 14:40:46 +0300

    Improve containers for responsiveness

--- src/components/shared/containers.ts
+++ src/components/shared/containers.ts
@@ -7,13 +7,24 @@ interface FlexProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLDiv
     align?: "flex-start" | "flex-end" | "center" | "stretch"
     equal?: boolean
     autoGrow?: boolean
+    // Used to automatically convert a row to column at specific media-width
+    breakAt?: number
+    centerAt?: number
 }
 export const Flex = styled.div<FlexProps>`
     display: flex;
     justify-content: ${(props) => (props.justify ? props.justify : "flex-start")};
     align-items: ${(props) => (props.align ? props.align : "stretch")};
     flex-basis: ${(props) => (props.equal ? "0" : "inherit")};
-    flex-grow: ${(props) => (props.equal || props.autoGrow ? "1" : "inherit")};
+    flex-grow: ${(props) => (props.equal || props.autoGrow ? "1" : "initial")};
+
+    @media (max-width: ${({ breakAt }) => (breakAt ? breakAt : 0)}px) {
+        flex-direction: column;
+    }
+    @media (max-width: ${({ centerAt }) => (centerAt ? centerAt : 0)}px) {
+        align-items: center;
+        justify-content: center;
+    }
 `
 
 export const FlexColumn = styled(Flex)<FlexProps>`
@@ -29,6 +40,12 @@ export const ConstrainedBody = styled.div<ConstrainedBodyProps>`
     @media (min-width: ${(props) => props.maxWidth}px) {
         padding-right: calc((100% - ${(props) => props.maxWidth}px) / 2);
         padding-left: calc((100% - ${(props) => props.maxWidth}px) / 2);
+    }
+`
+
+export const HideAt = styled.div<{ maxWidth: number }>`
+    @media (max-width: ${(props) => props.maxWidth}px) {
+        display: none;
     }
 `
 
--- src/data/theme.ts
+++ src/data/theme.ts
@@ -1,2 +1,5 @@
 export const NavBarHeight = 60
 export const SideBarWidth = 100
+
+export const primaryColor = "#03A9F4"
+export const secondaryColor = "#546E7A"
