141607ccb449b49479c8dd2ead95e3ce5b860c58
Billcountry <me@billcountry.tech>
2022-04-04 22:49:27 +0300

    Better mobile vs desktop

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,19 +1,53 @@
 import React from "react"
-import { Break, ConstrainedBody, Flex } from "components/shared/containers"
+import { Break, ConstrainedBody, Flex, FlexColumn } from "components/shared/containers"
 import { OurHome } from "./our-home"
 import { UserHome } from "./user-home"
+import { GlobalContext, GlobalState } from "data/state"
+import { observer } from "mobx-react"
+import { MobileBreakPoint } from "data/theme"
+import { Pivot, PivotItem } from "@fluentui/react"
 
-// style={{ backgroundColor: "#f1f1f1", height: "auto", margin: "0.5em" }}
+interface HomeState {
+    activeMobileView: "explore" | "learn" | string
+}
 
-export class Home extends React.Component {
+@observer
+export class Home extends React.Component<any, HomeState> {
+    static contextType = GlobalContext
+
+    constructor(props: any) {
+        super(props)
+        this.state = {
+            activeMobileView: "explore",
+        }
+    }
     render() {
+        const { activeMobileView } = this.state
+        const state: GlobalState = this.context
+        const showMobile = state.appWidth <= MobileBreakPoint
         return (
             <ConstrainedBody maxWidth={1400}>
-                <Flex breakAt={900} equal>
+                <Flex breakAt={MobileBreakPoint} equal>
                     <OurHome />
-                    <Break size={3} />
-                    <UserHome />
+                    {!showMobile && <Break size={3} />}
+                    {!showMobile && <UserHome />}
                 </Flex>
+                {showMobile && (
+                    <FlexColumn align="center">
+                        <Break size={1} />
+                        <Pivot
+                            selectedKey={activeMobileView}
+                            headersOnly
+                            onLinkClick={(item) => this.setState({ activeMobileView: `${item?.props.itemKey}` })}
+                        >
+                            <PivotItem headerText="Explore Promises" itemKey="explore" key="explore" />
+                            <PivotItem headerText="Learn More" itemKey="learn" key="learn" />
+                        </Pivot>
+                        <Break size={3} />
+                    </FlexColumn>
+                )}
+                {showMobile && activeMobileView === "explore" && <UserHome />}
+                <Break size={3} />
             </ConstrainedBody>
         )
     }
