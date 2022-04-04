2945d55ea696cab789ac07ec057fe64e0ee798c9
Billcountry <me@billcountry.tech>
2022-04-04 19:48:55 +0300

    Minimal view

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -2,27 +2,17 @@ import React from "react"
 import { Break, ConstrainedBody, Flex } from "components/shared/containers"
 import { OurHome } from "./our-home"
 import { UserHome } from "./user-home"
-import { Label, Pivot, PivotItem } from "@fluentui/react"
+
+// style={{ backgroundColor: "#f1f1f1", height: "auto", margin: "0.5em" }}
 
 export function Home() {
     return (
         <ConstrainedBody maxWidth={1400}>
             <Flex breakAt={900} equal>
                 <OurHome />
-                <Break size={0.3} style={{ backgroundColor: "#f1f1f1", height: "auto", margin: "0.5em" }} />
+                <Break size={3} />
                 <UserHome />
             </Flex>
-            <Pivot>
-                <PivotItem headerText="Promise Feed">
-                    <Label>A list of promises made recently to the currently selected entity.</Label>
-                </PivotItem>
-                <PivotItem headerText="Institutions">
-                    <Label>Institutions relating to the currently selected entity.</Label>
-                </PivotItem>
-                <PivotItem headerText="Subdivisions">
-                    <Label>Sub Divisions of the current entity if applicable.</Label>
-                </PivotItem>
-            </Pivot>
         </ConstrainedBody>
     )
 }
--- src/components/home/user-home.tsx
+++ src/components/home/user-home.tsx
@@ -1,20 +1,38 @@
 import React from "react"
-import { Break, Flex, FlexColumn } from "components/shared/containers"
-import { ScalableImage } from "components/shared/images"
+import styled from "styled-components"
+import { FlexColumn } from "components/shared/containers"
 import { Text } from "@fluentui/react"
 import { PromiseStats } from "components/charts/promises"
+import { primaryColor, TitleFonts } from "data/theme"
+import { Link } from "react-router-dom"
 
 export function UserHome() {
     return (
         <FlexColumn autoGrow centerAt={800} justify="flex-start">
-            <Flex justify="flex-end" align="center">
-                <Text variant="xxLarge">Our Planet</Text>
-                <Break />
-                <ScalableImage maxHeight={120} minHeight={60} relativeHeight={"10vw"} alt="" src="/assets/Simple_Globe.svg" />
-            </Flex>
-            <Text variant="large">The only planet we call home, let us make sure that it's under the right hands.</Text>
+            <Banner>
+                <img alt="" src="/assets/Simple_Globe.svg" />
+                The World
+            </Banner>
 
             <PromiseStats data={{ broken: 647, pending: 123, inProgress: 39, delivered: 215 }} height={240} />
+            <Text variant="mediumPlus">
+                The World has a high rate of <strong>broken promises</strong>. It's our hope that you'll use this information to
+                help correct this trend. <Link to="/">See what you can do...</Link>
+            </Text>
         </FlexColumn>
     )
 }
+
+const Banner = styled.div`
+    display: flex;
+    color: ${primaryColor};
+    font-family: ${TitleFonts};
+    font-size: 2em;
+    font-style: oblique;
+    align-items: center;
+    margin-bottom: 16px;
+    img {
+        margin-right: 5px;
+        height: 40px;
+    }
+`
