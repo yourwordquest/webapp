2ec9fae29ef02cf622b6136d9b3f399d073928b7
Billcountry <me@billcountry.tech>
2022-04-04 14:31:46 +0300

    Set global state and setup react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
 import React from "react"
+import { BrowserRouter } from "react-router-dom"
 import { Home } from "components/home"
 import { GlobalStyle } from "styles"
 import { NavBar } from "components/navigation/navbar"
 import { ThemeProvider } from "@fluentui/react"
 import { FluentTheme } from "data/theme"
+import { GlobalContext, GlobalState } from "data/state"
 
 function App() {
+    const sharedState = new GlobalState()
     return (
-        <ThemeProvider theme={FluentTheme}>
-            <GlobalStyle />
-            <NavBar />
-            <Home />
-        </ThemeProvider>
+        <BrowserRouter>
+            <GlobalContext.Provider value={sharedState}>
+                <ThemeProvider theme={FluentTheme}>
+                    <GlobalStyle />
+                    <NavBar />
+                    <Home />
+                </ThemeProvider>
+            </GlobalContext.Provider>
+        </BrowserRouter>
     )
 }
 
