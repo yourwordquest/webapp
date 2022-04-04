951384517da65e38bc677b40f115a15aeeceade6
Billcountry <me@billcountry.tech>
2022-04-04 21:46:57 +0300

    Make home a class component

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -5,14 +5,16 @@ import { UserHome } from "./user-home"
 
 // style={{ backgroundColor: "#f1f1f1", height: "auto", margin: "0.5em" }}
 
-export function Home() {
-    return (
-        <ConstrainedBody maxWidth={1400}>
-            <Flex breakAt={900} equal>
-                <OurHome />
-                <Break size={3} />
-                <UserHome />
-            </Flex>
-        </ConstrainedBody>
-    )
+export class Home extends React.Component {
+    render() {
+        return (
+            <ConstrainedBody maxWidth={1400}>
+                <Flex breakAt={900} equal>
+                    <OurHome />
+                    <Break size={3} />
+                    <UserHome />
+                </Flex>
+            </ConstrainedBody>
+        )
+    }
 }
