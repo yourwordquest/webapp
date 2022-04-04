2aa3861fdc9e65fa9aa4d9421ca1c2de0e98f84d
Billcountry <me@billcountry.tech>
2022-04-04 12:40:55 +0300

    Set fluent UI theme and use specified fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from "react"
 import { Home } from "components/home"
 import { GlobalStyle } from "styles"
+import { NavBar } from "components/navigation/navbar"
+import { ThemeProvider } from "@fluentui/react"
+import { FluentTheme } from "data/theme"
 
 function App() {
     return (
-        <>
+        <ThemeProvider theme={FluentTheme}>
             <GlobalStyle />
+            <NavBar />
             <Home />
-        </>
+        </ThemeProvider>
     )
 }
 
--- src/data/theme.ts
+++ src/data/theme.ts
@@ -1,3 +1,5 @@
+import { createTheme } from "@fluentui/react"
+
 export const NavBarHeight = 60
 export const SideBarWidth = 100
 
@@ -10,3 +12,51 @@ export const promiseColors = {
     pending: "#ffc107",
     broken: "#b71c1c",
 }
+
+const TextFonts =
+    "'Roboto', 'Segoe UI', 'Segoe UI Web (West European)', 'Segoe UI', -apple-system, BlinkMacSystemFont, 'Helvetica Neue', sans-serif"
+const TitleFonts =
+    "'Lora', 'Segoe UI', 'Segoe UI Web (West European)', 'Segoe UI', -apple-system, BlinkMacSystemFont, 'Helvetica Neue', sans-serif"
+
+// Partly generated with
+// https://fluentuipr.z22.web.core.windows.net/heads/master/theming-designer/index.html
+export const FluentTheme = createTheme({
+    palette: {
+        themePrimary: "#0099ff",
+        themeLighterAlt: "#f5fbff",
+        themeLighter: "#d6efff",
+        themeLight: "#b3e0ff",
+        themeTertiary: "#66c2ff",
+        themeSecondary: "#1fa5ff",
+        themeDarkAlt: "#008ae6",
+        themeDark: "#0074c2",
+        themeDarker: "#00568f",
+        neutralLighterAlt: "#faf9f8",
+        neutralLighter: "#f3f2f1",
+        neutralLight: "#edebe9",
+        neutralQuaternaryAlt: "#e1dfdd",
+        neutralQuaternary: "#d0d0d0",
+        neutralTertiaryAlt: "#c8c6c4",
+        neutralTertiary: "#c7c7c7",
+        neutralSecondary: "#8f8f8f",
+        neutralPrimaryAlt: "#5b5b5b",
+        neutralPrimary: "#444444",
+        neutralDark: "#343434",
+        black: "#272727",
+        white: "#ffffff",
+    },
+    fonts: {
+        tiny: { fontFamily: TextFonts },
+        small: { fontFamily: TextFonts },
+        xSmall: { fontFamily: TextFonts },
+        smallPlus: { fontFamily: TextFonts },
+        medium: { fontFamily: TextFonts },
+        mediumPlus: { fontFamily: TextFonts },
+        large: { fontFamily: TitleFonts },
+        xLarge: { fontFamily: TitleFonts },
+        xxLarge: { fontFamily: TitleFonts },
+        xxLargePlus: { fontFamily: TitleFonts },
+        superLarge: { fontFamily: TitleFonts },
+        mega: { fontFamily: TitleFonts },
+    },
+})
