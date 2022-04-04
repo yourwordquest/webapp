e3edfe5e916a704a5833c333d97869ccb4380f2d
Billcountry <me@billcountry.tech>
2022-04-04 21:09:19 +0300

    More relevant and attention capturing wording

--- src/components/home/our-home.tsx
+++ src/components/home/our-home.tsx
@@ -1,32 +1,31 @@
 import React from "react"
-import { Break, FlexColumn } from "components/shared/containers"
-import { Text } from "@fluentui/react"
+import { Break, FlexColumn, Flex } from "components/shared/containers"
 import { observer } from "mobx-react"
 import styled from "styled-components"
-import { primaryColor, TitleFonts } from "data/theme"
+import { MobileBreakPoint, primaryColor, secondaryColor, TitleFonts } from "data/theme"
 
 export const OurHome = observer(() => {
     return (
-        <FlexColumn autoGrow centerAt={800}>
+        <FlexColumn autoGrow centerAt={MobileBreakPoint}>
             <Banner>
                 <img alt="" src="/icon.svg" />
                 <span>YourWord.Quest</span>
             </Banner>
 
-            <Text variant="xLarge">We track promises made to humans by humans; for the sake of humanity.</Text>
-            <Break size={0.5} />
+            <Slogan>
+                <Flex>&ldquo;We must accept finite disappointment, but never lose infinite hope.&rdquo;</Flex>
+                <Flex justify="center"> - Dr. Martin Luther King Jr</Flex>
+            </Slogan>
+            <Break size={1} />
 
-            <Text style={{ textAlign: "justify" }} variant="mediumPlus">
-                We live in a world where our leaders from local to world leaders say one thing and then go ahead and do another.
-                This is especially common during campaign seasons when promises are made, forgotten then we repeat the same
-                cycle again the next season.
-            </Text>
-            <Break size={0.5} />
-            <Text style={{ textAlign: "justify" }} variant="mediumPlus">
-                We aim to provide information to you in a more structured way, who made a promise, when and did they keep it?
-                &nbsp;<a href="/about">Read all about our mission</a>&nbsp; or jump right in and see how promises that concern
-                you are performing.
-            </Text>
+            <Content>
+                In a world full of broken promises, we aim to restore the hope of a better future by making sure that:
+                <ul>
+                    <li>Promises don't get lost in our news cycles</li>
+                    <li>Words can be associated with their original source</li>
+                    <li>We provide actionable information to the public</li>
+                </ul>
+            </Content>
         </FlexColumn>
     )
 })
@@ -39,8 +38,26 @@ const Banner = styled.div`
     font-style: oblique;
     align-items: center;
     margin-bottom: 16px;
+    user-select: none;
     img {
         margin-right: 5px;
         height: 40px;
+    }
+`
+
+const Slogan = styled.div`
+    color: ${secondaryColor};
+    font-family: ${TitleFonts};
+    font-size: 1.5em;
+    font-style: oblique;
+`
+
+const Content = styled.div`
+    font-size: 1.4em;
+    font-family: ${TitleFonts};
+    text-align: justify;
+    li {
+        font-style: oblique;
+        text-align: left;
     }
 `
