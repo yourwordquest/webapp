f2acddd0e882b7a8f54f01364554b8f1c3322ac4
Billcountry <me@billcountry.tech>
2022-04-05 19:02:35 +0300

    Add and initialize firebase

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,22 @@ import "./index.css"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
 import { initializeIcons } from "@fluentui/font-icons-mdl2"
+import { initializeApp } from "firebase/app"
+import { initializeAnalytics } from "firebase/analytics"
 
+// Initialize firebase
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+}
+
+const app = initializeApp(firebaseConfig)
+initializeAnalytics(app)
 initializeIcons()
 
 ReactDOM.render(
