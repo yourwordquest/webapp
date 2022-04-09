d856ea9fc51be8e83823cdaaff5ddbcbb8651fbb
Billcountry <me@billcountry.tech>
2022-04-09 13:25:54 +0300

    Support development locally and when deployed

--- src/utils/routing.tsx
+++ src/utils/routing.tsx
@@ -1,11 +1,20 @@
 import React from "react"
-import { Location, NavigateFunction, useLocation, useNavigate, useParams } from "react-router-dom"
+import {
+    Location,
+    NavigateFunction,
+    URLSearchParamsInit,
+    useLocation,
+    useNavigate,
+    useParams,
+    useSearchParams,
+} from "react-router-dom"
 
 export interface RoutedProps<Params = any, Search = any> {
     location: Location
     navigate: NavigateFunction
     params: Params
     search: Search
+    setSearchParams: URLSearchParamsInit
 }
 
 export function withRouter<P extends RoutedProps>(Child: React.ComponentClass<P>) {
@@ -14,10 +23,33 @@ export function withRouter<P extends RoutedProps>(Child: React.ComponentClass<P>
         const navigate = useNavigate()
         const params = useParams()
         const search: Record<string, string> = {}
-        const search_params = new URLSearchParams(location.search)
+        const [search_params, setSearchParams] = useSearchParams()
         for (const [key, value] of search_params.entries()) {
             search[key] = value
         }
-        return <Child {...(props as P)} navigate={navigate} location={location} params={params} search={search} />
+        return (
+            <Child
+                {...(props as P)}
+                navigate={navigate}
+                location={location}
+                params={params}
+                search={search}
+                setSearchParams={setSearchParams}
+            />
+        )
     }
+}
+
+export function make_api_url(path: string): string {
+    if (path.startsWith("https://") || path.startsWith("http://")) {
+        return path
+    }
+    if (!path.startsWith("/")) {
+        path = `/${path}`
+    }
+    let host = "https://api.yourword.quest"
+    if (window.location.port === "3000") {
+        host = `${window.location.protocol}//${window.location.hostname}:8000`
+    }
+    return `${host}${path}`
 }
