c5e5b8884fd4643a1ad72732a069cdb0835e091e
Billcountry <me@billcountry.tech>
2022-04-12 21:11:07 +0300

    Items view and descriptions

--- src/components/contributions/index.tsx
+++ src/components/contributions/index.tsx
@@ -2,22 +2,79 @@ import React from "react"
 import styled from "styled-components"
 import { Break, ConstrainedBody, Flex, FlexColumn } from "components/shared/containers"
 import { MobileBreakPoint, primaryColor, TitleFonts } from "data/theme"
-import { PrimaryButton, Text } from "@fluentui/react"
+import { Icon, PrimaryButton, Text } from "@fluentui/react"
 import { GlobalContext, GlobalState } from "data/state"
 import { observer } from "mobx-react"
+import { RoutedProps, withRouter } from "utils/routing"
 
 interface ContributionsState {
     active_tab: "new_contribution" | "contributions" | string
+    contrib_item: string
 }
 
+interface ContribItem {
+    title: string
+    key: string
+    description: string
+    icon: string
+}
+
+function contrib_item(key: string, title: string, description: string, icon: string): ContribItem {
+    return { key, title, description, icon }
+}
+
+const contrib_items: ContribItem[] = [
+    contrib_item(
+        "promise",
+        "A Promise",
+        "A promise made by a leader in representation of self, a company or organization.",
+        ""
+    ),
+    contrib_item(
+        "event",
+        "An Event",
+        "An event that happened and is meaningful to certain promises people or organizations.",
+        ""
+    ),
+    contrib_item(
+        "location",
+        "Location Details",
+        "A location that we don't have in our database or updated information regarding the location.",
+        ""
+    ),
+    contrib_item(
+        "person",
+        "Person Details",
+        "An influential person not in our database or updated information regarding them.",
+        ""
+    ),
+    contrib_item(
+        "org",
+        "Organization Details",
+        "An influential organization not in our database or updated information regarding them.",
+        ""
+    ),
+    contrib_item(
+        "office",
+        "Office Details",
+        "An office belongs to an organization and is occupied by a person, you can provide information about a new office or an update to an existing one.",
+        ""
+    ),
+]
+
 @observer
-export class Contributions extends React.Component<any, ContributionsState> {
+class RoutedContributions extends React.Component<RoutedProps<any, { loc?: string }>, ContributionsState> {
     static contextType = GlobalContext
 
     render() {
-        const { active_tab = "new_contribution" } = this.state || {}
+        const { active_tab = "new_contribution", contrib_item } = this.state || {}
+        const {
+            navigate,
+            search: { loc },
+        } = this.props
         const state: GlobalState = this.context
         const showMobile = state.appWidth <= MobileBreakPoint
+        const query = loc ? `?loc=${loc}` : ""
         return (
             <StyledContributions maxWidth={1400}>
                 {showMobile && (
@@ -42,6 +99,32 @@ export class Contributions extends React.Component<any, ContributionsState> {
                     {Boolean(!showMobile || active_tab === "new_contribution") && (
                         <FlexColumn className="section" autoGrow>
                             <Intro>Hello, what would you like to contribute today?</Intro>
+                            <ContributionOptions>
+                                {contrib_items.map((item) => (
+                                    <Flex
+                                        onClick={() => this.setState({ contrib_item: item.key })}
+                                        className={`option ${contrib_item === item.key ? "selected" : ""}`}
+                                        key={item.key}
+                                        align="center"
+                                    >
+                                        <Icon
+                                            className="radio"
+                                            iconName={contrib_item === item.key ? "RadioBtnOn" : "RadioBtnOff"}
+                                        />
+                                        <FlexColumn autoGrow>
+                                            <div className="title">{item.title}</div>
+                                            <small>{item.description}</small>
+                                        </FlexColumn>
+                                    </Flex>
+                                ))}
+                                <Flex justify="flex-end">
+                                    <PrimaryButton
+                                        disabled={!contrib_item}
+                                        text="Proceed"
+                                        onClick={() => navigate(`/contribute/${contrib_item}${query}`)}
+                                    />
+                                </Flex>
+                            </ContributionOptions>
                         </FlexColumn>
                     )}
 
@@ -68,10 +151,14 @@ export class Contributions extends React.Component<any, ContributionsState> {
                         </FlexColumn>
                     )}
                 </Flex>
+                {/* On mobile make sure that we can scroll the button above the chat button */}
+                <Break size={5} />
             </StyledContributions>
         )
     }
 }
+
+export const Contributions = withRouter<RoutedProps>(RoutedContributions)
 
 const Intro = styled.div`
     font-size: 1.5em;
@@ -101,6 +188,32 @@ const StyledContributions = styled(ConstrainedBody)`
             background-color: #fafafa;
             margin: 0.5em;
             padding: 0.5em;
+        }
+    }
+`
+
+const ContributionOptions = styled(FlexColumn)`
+    .option {
+        padding: 1em 0.5em;
+        background-color: #ffffff;
+        margin: 0.5em 0;
+        border: 1px solid #f1f1f1;
+        border-radius: 5px;
+        font-size: 16px;
+        cursor: pointer;
+        .radio {
+            margin-right: 0.5em;
+            font-size: 1.3em;
+        }
+        &:hover {
+            background-color: ${primaryColor}11;
+        }
+        &.selected {
+            border: 1px solid ${primaryColor};
+            .radio,
+            .title {
+                color: ${primaryColor};
+            }
         }
     }
 `
